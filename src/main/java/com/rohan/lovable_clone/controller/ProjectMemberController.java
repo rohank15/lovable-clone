74b4f65cbb80c5033a746522adb123b4314edc2a
rohank15 <rohanrk124@gmail.com>
2026-06-12 03:30:46 +0530

    Added missing paranthesis in RequestMapping Annotation

--- src/main/java/com/rohan/lovable_clone/controller/ProjectMemberController.java
+++ src/main/java/com/rohan/lovable_clone/controller/ProjectMemberController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/projects/{projectId/members")
+@RequestMapping("/api/projects/{projectId}/members")
 @RequiredArgsConstructor
 public class ProjectMemberController {
     private final ProjectMemberService projectMemberService;
