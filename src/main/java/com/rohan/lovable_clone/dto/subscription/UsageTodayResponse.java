0d666a330d1c1f1848aeb38f2ddab008a919456d
rohank15 <rohanrk124@gmail.com>
2026-06-11 21:48:40 +0530

    Changed Primitive Data Types to Wrapper Classes

--- src/main/java/com/rohan/lovable_clone/dto/subscription/UsageTodayResponse.java
+++ src/main/java/com/rohan/lovable_clone/dto/subscription/UsageTodayResponse.java
@@ -1,9 +1,9 @@
 package com.rohan.lovable_clone.dto.subscription;
 
 public record UsageTodayResponse(
-        int tokensUsed,
-        int tokensLimit,
-        int previewRunning,
-        int previewsLimit
+        Integer tokensUsed,
+        Integer tokensLimit,
+        Integer previewRunning,
+        Integer previewsLimit
 ) {
 }
