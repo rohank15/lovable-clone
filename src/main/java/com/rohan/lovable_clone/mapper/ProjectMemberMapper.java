94d863a1820148e13e1c87b677093a775073bb2a
rohank15 <rohanrk124@gmail.com>
2026-06-14 04:02:29 +0530

    Mapper for Project and ProjectMember.

---
+++ src/main/java/com/rohan/lovable_clone/mapper/ProjectMemberMapper.java
@@ -0,0 +1,22 @@
+package com.rohan.lovable_clone.mapper;
+
+import com.rohan.lovable_clone.dto.member.MemberResponse;
+import com.rohan.lovable_clone.entity.ProjectMember;
+import com.rohan.lovable_clone.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProjectMemberMapper {
+
+    @Mapping(target = "userId", source = "id")
+    @Mapping(target = "projectRole", constant = "OWNER")
+    MemberResponse toProjectMemberResponseFromOwner(User owner);
+
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "name", source = "user.name")
+    MemberResponse toProjectMemberResponseFromMember(ProjectMember projectMember);
+}
