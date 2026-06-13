ddc10e4baddd8c80a80d3e98bd9dfdb34cb0b163
rohank15 <rohanrk124@gmail.com>
2026-06-14 04:01:03 +0530

    Added business logic code to Services

--- src/main/java/com/rohan/lovable_clone/service/impl/ProjectMemberServiceImpl.java
+++ src/main/java/com/rohan/lovable_clone/service/impl/ProjectMemberServiceImpl.java
@@ -3,30 +3,119 @@ package com.rohan.lovable_clone.service.impl;
 import com.rohan.lovable_clone.dto.member.InviteMemberRequest;
 import com.rohan.lovable_clone.dto.member.MemberResponse;
 import com.rohan.lovable_clone.dto.member.UpdateMemberRoleRequest;
+import com.rohan.lovable_clone.entity.Project;
+import com.rohan.lovable_clone.entity.ProjectMember;
+import com.rohan.lovable_clone.entity.ProjectMemberId;
+import com.rohan.lovable_clone.entity.User;
+import com.rohan.lovable_clone.mapper.ProjectMemberMapper;
+import com.rohan.lovable_clone.repository.ProjectMemberRepository;
+import com.rohan.lovable_clone.repository.ProjectRepository;
+import com.rohan.lovable_clone.repository.UserRepository;
 import com.rohan.lovable_clone.service.ProjectMemberService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class ProjectMemberServiceImpl implements ProjectMemberService {
+
+    private final ProjectRepository projectRepository;
+    private final ProjectMemberMapper projectMemberMapper;
+    private final ProjectMemberRepository projectMemberRepository;
+    private final UserRepository userRepository;
+
     @Override
     public List<MemberResponse> getProjectMembers(Long userId, Long projectId) {
-        return List.of();
+        Project project = getAccessibleProjectById(projectId, userId);
+
+        List<MemberResponse> memberResponseList = new ArrayList<>();
+        memberResponseList.add(projectMemberMapper.toProjectMemberResponseFromOwner(project.getOwner()));
+
+        memberResponseList.addAll(
+                projectMemberRepository.findByProjectId(projectId)
+                        .stream()
+                        .map(projectMemberMapper::toProjectMemberResponseFromMember)
+                        .toList());
+
+        return memberResponseList;
     }
 
     @Override
     public MemberResponse inviteMember(Long projectId, InviteMemberRequest request, Long userId) {
-        return null;
+        Project project = getAccessibleProjectById(projectId, userId);
+
+        if(!project.getOwner().getId().equals(userId)) {
+            throw new RuntimeException("Not allowed");
+        }
+
+        User invitee = userRepository.findByEmail(request.email()).orElseThrow();
+
+        if(invitee.getId().equals(userId)) {
+            throw new RuntimeException("Cannot invite yourself");
+        }
+
+        ProjectMemberId projectMemberId = new ProjectMemberId(projectId, invitee.getId());
+
+        if(projectMemberRepository.existsById(projectMemberId)) {
+            throw new RuntimeException("Cannot invite once again");
+        }
+
+        ProjectMember member = ProjectMember.builder()
+                .id(projectMemberId)
+                .project(project)
+                .user(invitee)
+                .projectRole(request.role())
+                .invitedAt(Instant.now())
+                .build();
+
+        projectMemberRepository.save(member);
+
+        return projectMemberMapper.toProjectMemberResponseFromMember(member);
     }
 
     @Override
     public MemberResponse updateMemberRole(Long projectId, Long memberId, UpdateMemberRoleRequest request, Long userId) {
-        return null;
+        Project project = getAccessibleProjectById(projectId, userId);
+
+        if(!project.getOwner().getId().equals(userId)) {
+            throw new RuntimeException("Not allowed");
+        }
+
+        ProjectMemberId projectMemberId = new ProjectMemberId(projectId, memberId);
+        ProjectMember member = projectMemberRepository.findById(projectMemberId).orElseThrow();
+
+        member.setProjectRole(request.role());
+
+        projectMemberRepository.save(member);
+
+        return projectMemberMapper.toProjectMemberResponseFromMember(member);
     }
 
     @Override
-    public MemberResponse deleteProjectMember(Long projectId, Long memberId, Long userId) {
-        return null;
+    public void removeProjectMember(Long projectId, Long memberId, Long userId) {
+        Project project = getAccessibleProjectById(projectId, userId);
+
+        if(!project.getOwner().getId().equals(userId)) {
+            throw new RuntimeException("Not allowed");
+        }
+
+        ProjectMemberId projectMemberId = new ProjectMemberId(projectId, memberId);
+        if(!projectMemberRepository.existsById(projectMemberId)) {
+            throw new RuntimeException("Member not found in project");
+        }
+
+        projectMemberRepository.deleteById(projectMemberId);
+    }
+
+    /// INTERNAL FUNCTIONS
+
+    public Project getAccessibleProjectById(Long id, Long userId) {
+        return  projectRepository.findAccessibleProjectById(id, userId).orElseThrow();
     }
 }
--- src/main/java/com/rohan/lovable_clone/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/rohan/lovable_clone/service/impl/ProjectServiceImpl.java
@@ -1,37 +1,94 @@
 package com.rohan.lovable_clone.service.impl;
 
+import com.rohan.lovable_clone.entity.Project;
+import com.rohan.lovable_clone.entity.User;
+import com.rohan.lovable_clone.mapper.ProjectMapper;
+import com.rohan.lovable_clone.repository.ProjectRepository;
 import com.rohan.lovable_clone.dto.project.ProjectRequest;
 import com.rohan.lovable_clone.dto.project.ProjectResponse;
 import com.rohan.lovable_clone.dto.project.ProjectSummaryResponse;
+import com.rohan.lovable_clone.repository.UserRepository;
 import com.rohan.lovable_clone.service.ProjectService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    private final ProjectMapper projectMapper;
+
+    @Override
+    public ProjectResponse createProject(ProjectRequest request, Long userId) {
+        User owner = userRepository.findById(userId).orElseThrow();
+
+        Project project = Project.builder()
+                .name(request.name())
+                .owner(owner)
+                .isPublic(false)
+                .build();
+
+        project = projectRepository.save(project);
+
+        return projectMapper.toProjectResponse(project);
+    }
+
     @Override
     public List<ProjectSummaryResponse> getUserProjects(Long userId) {
-        return List.of();
+//        return projectRepository.findAllAccessibleByUser(userId)
+//                .stream()
+//                .map(project -> projectMapper.toProjectSummaryResponse(project))
+//                .collect(Collectors.toList());
+
+        List<Project> projects = projectRepository.findAllAccessibleByUser(userId);
+
+        return projectMapper.toListOfProjectSummaryResponse(projects);
     }
 
     @Override
     public ProjectResponse getUserProjectById(Long id, Long userId) {
-        return null;
-    }
-
-    @Override
-    public ProjectResponse createProject(ProjectRequest request, Long userId) {
-        return null;
+        Project project = getAccessibleProjectById(id, userId);
+        return projectMapper.toProjectResponse(project);
     }
 
     @Override
     public ProjectResponse updateProject(Long id, ProjectRequest request, Long userId) {
-        return null;
+        Project project = getAccessibleProjectById(id, userId);
+
+        if(!project.getOwner().getId().equals(userId)) {
+            throw new RuntimeException("You are not allowed to update the name");
+        }
+
+        project.setName(request.name());
+        project = projectRepository.save(project);
+
+        return projectMapper.toProjectResponse(project);
     }
 
     @Override
     public void softDelete(Long id, Long userId) {
+        Project project = getAccessibleProjectById(id, userId);
 
+        if(!project.getOwner().getId().equals(userId)) {
+            throw new RuntimeException("You are not allowed to delete this project");
+        }
+
+        project.setDeletedAt(Instant.now());
+
+        projectRepository.save(project);
+    }
+
+    /// INTERNAL FUNCTIONS
+
+    public Project getAccessibleProjectById(Long id, Long userId) {
+        return  projectRepository.findAccessibleProjectById(id, userId).orElseThrow();
     }
 }
