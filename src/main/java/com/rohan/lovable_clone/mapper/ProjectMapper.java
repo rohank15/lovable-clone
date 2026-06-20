049c732fa0e878e0e340191ecec30ed8380b6813
rohank15 <rohanrk124@gmail.com>
2026-06-20 09:39:49 +0530

    Updated mapper according to entity and dto changes.

--- src/main/java/com/rohan/lovable_clone/mapper/ProjectMapper.java
+++ src/main/java/com/rohan/lovable_clone/mapper/ProjectMapper.java
@@ -4,6 +4,7 @@ import com.rohan.lovable_clone.dto.project.ProjectResponse;
 import com.rohan.lovable_clone.dto.project.ProjectSummaryResponse;
 import com.rohan.lovable_clone.entity.Project;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public interface ProjectMapper {
     ProjectResponse toProjectResponse(Project project);
 
+    @Mapping(target = "projectName", source = "name")
     ProjectSummaryResponse toProjectSummaryResponse(Project project);
 
     List<ProjectSummaryResponse> toListOfProjectSummaryResponse(List<Project> projects);
